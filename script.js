bf94e86d9d7fe4201690e37fa1b52bd004fddf17
Ashley Park <ashp119@berkeley.edu>
2022-05-05 05:34:48 -0700

    worked on questions

--- script.js
+++ script.js
@@ -3,49 +3,59 @@
 const Questions = [{
     id: 0,
     q: "what activity sounds most appealing to you right now?",
-    a: [{ text: "sitting on the couch eating ice cream", isCorrect: false },
-        { text: "going to a club aye", isCorrect: false },
-        { text: "taking a long drive", isCorrect: true },
-        { text: "karaoke!", isCorrect: false }
+    a: [{ text: "sitting on the couch eating ice cream", image: "", isCorrect: false },
+        { text: "going to a club aye", image: "", isCorrect: false },
+        { text: "taking a long drive", image: "", isCorrect: true },
+        { text: "taking a long drive", image: "", isCorrect: true },
+        { text: "taking a long drive", image: "", isCorrect: true },
+        { text: "karaoke!", image: "", isCorrect: false }
     ]
 
 },
 {
     id: 1,
-    q: "What color represents your current mood?",
-    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
-        { text: "phuket", isCorrect: false },
-        { text: "Ayutthaya", isCorrect: false },
-        { text: "Bangkok", isCorrect: true }
+    q: "What drink would you want right now? ",
+    a: [{ text: "Hot drink (cocoa, tea) ", image: "", isCorrect: false, isSelected: false },
+        { text: "Shots shots shots ", image: "", isCorrect: false },
+        { text: "Chill drink (beer, cider)", mage: "", isCorrect: false },
+        { text: "Boba !", image: "", isCorrect: true },
+        { text: "Coffee", image: ""},
+        { text: "Energy drink", image: ""}
     ]
 
 },
 {
     id: 2,
-    q: "What activity sounds most appealing?",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "Location check?",
+    a: [{ text: "Doe Library", image: "", isCorrect: false },
+        { text: "Strada", image: "", isCorrect: false },
+        { text: "Kips (ew)", image: "", isCorrect: true },
+        { text: "Jaguar", image: "", isCorrect: false },
+        { text: "Carol's House :)", image: ""},
+        { text: "Glade", image: ""}
     ]
 
 },
 {
     id: 3,
-    q: "How sensitive would you be to loud noise in this moment?",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+    q: "What emoji are you?",
+    a: [{ text: "", image: 'emoji-images/answer1.png', isCorrect: false },
+        { text: "", image: 'emoji-images/answer2.png', isCorrect: false },
+        { text: "", image: 'emoji-images/answer3.png', isCorrect: true },
+        { text: "", image: 'emoji-images/answer4.png', isCorrect: true },
+        { text: "", image: 'emoji-images/answer5.png', isCorrect: true },
+        { text: "", image: 'emoji-images/answer6.png', isCorrect: false }
     ]
 
 },
 {
     id: 4,
-    q: "How appealing does dancing sound right now",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
+    q: "How many people do you wanna be around?",
+    a: [{ text: "No one (free spirit)", isCorrect: false },
+        { text: "Hundreds if not thousands", isCorrect: false },
+        { text: "Just some amigos", isCorrect: true },
+        { text: "Just some amigos", isCorrect: true },
+        { text: "Just some amigos", isCorrect: true },
         { text: "rajkot", isCorrect: false }
     ]
 
@@ -85,13 +95,17 @@ const op1 = document.getElementById('op1');
 const op2 = document.getElementById('op2');
 const op3 = document.getElementById('op3');
 const op4 = document.getElementById('op4');
+const op5 = document.getElementById('op5');
+const op6 = document.getElementById('op6');
 
 
 // Providing option text 
-op1.innerText = Questions[id].a[0].text;
-op2.innerText = Questions[id].a[1].text;
-op3.innerText = Questions[id].a[2].text;
-op4.innerText = Questions[id].a[3].text;
+op1.innerHTML = Questions[id].a[0].text + '<img src="'+Questions[id].a[0].image+'">';
+op2.innerHTML = Questions[id].a[1].text + '<img src="'+Questions[id].a[1].image+'">';
+op3.innerHTML = Questions[id].a[2].text + '<img src="'+Questions[id].a[2].image+'">';
+op4.innerHTML = Questions[id].a[3].text + '<img src="'+Questions[id].a[3].image+'">';
+op5.innerHTML = Questions[id].a[4].text + '<img src="'+Questions[id].a[4].image+'">';
+op6.innerHTML = Questions[id].a[5].text + '<img src="'+Questions[id].a[5].image+'">';
 
 // Providing the true or false value to the options
 op1.value = Questions[id].a[0].isCorrect;
@@ -107,6 +121,8 @@ op1.addEventListener("click", () => {
     op2.style.backgroundColor = "lightskyblue";
     op3.style.backgroundColor = "lightskyblue";
     op4.style.backgroundColor = "lightskyblue";
+    op5.style.backgroundColor = "lightskyblue";
+    op6.style.backgroundColor = "lightskyblue";
     selected = op1.value;
 })
 
@@ -116,6 +132,8 @@ op2.addEventListener("click", () => {
     op2.style.backgroundColor = "lightgoldenrodyellow";
     op3.style.backgroundColor = "lightskyblue";
     op4.style.backgroundColor = "lightskyblue";
+    op5.style.backgroundColor = "lightskyblue";
+    op6.style.backgroundColor = "lightskyblue";
     selected = op2.value;
 })
 
@@ -125,6 +143,8 @@ op3.addEventListener("click", () => {
     op2.style.backgroundColor = "lightskyblue";
     op3.style.backgroundColor = "lightgoldenrodyellow";
     op4.style.backgroundColor = "lightskyblue";
+    op5.style.backgroundColor = "lightskyblue";
+    op6.style.backgroundColor = "lightskyblue";
     selected = op3.value;
 })
 
@@ -134,7 +154,31 @@ op4.addEventListener("click", () => {
     op2.style.backgroundColor = "lightskyblue";
     op3.style.backgroundColor = "lightskyblue";
     op4.style.backgroundColor = "lightgoldenrodyellow";
+    op5.style.backgroundColor = "lightskyblue";
+    op6.style.backgroundColor = "lightskyblue";
     selected = op4.value;
+})
+
+// Show selection for op5
+op4.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    op5.style.backgroundColor = "lightgoldenrodyellow";
+    op6.style.backgroundColor = "lightskyblue";
+    selected = op5.value;
+})
+
+// Show selection for op6
+op4.addEventListener("click", () => {
+    op1.style.backgroundColor = "lightskyblue";
+    op2.style.backgroundColor = "lightskyblue";
+    op3.style.backgroundColor = "lightskyblue";
+    op4.style.backgroundColor = "lightskyblue";
+    op5.style.backgroundColor = "lightskyblue";
+    op6.style.backgroundColor = "lightgoldenrodyellow";
+    selected = op6.value;
 })
 
 // Grabbing the evaluate button
@@ -162,7 +206,7 @@ var id = 0;
 
 next.addEventListener("click", () => {
 start = false;
-if (id < 2) {
+if (id < 4) {
     id++;
     iterate(id);
     console.log(id);
